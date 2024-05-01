8725d18e5ceb5481277cd767b9a965c10704464b
yodh4 <cyrilus2004@gmail.com>
2024-05-02 02:28:30 +0700

    Add front end

---
+++ main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from main.views import show_finalisasi_paket, show_hasil_cari, show_langganan_paket,show_riwayat_langganan
+from main.views import show_downloaded_song, show_delete_song
+app_name = 'main'
+
+urlpatterns = [
+    path('langganan/', show_langganan_paket, name='show_langganan_paket'),
+    path('langganan/<str:jenis>/<int:harga>/', show_finalisasi_paket, name='show_finalisasi_paket'),
+    path('langganan/riwayat-langganan/', show_riwayat_langganan, name='show_riwayat_langganan'),
+    path('search-result/<str:query>/', show_hasil_cari, name='show_hasil_cari'),
+    path('downloaded-song/', show_downloaded_song, name='show_downloaded_song'),
+    path('downloaded-song/delete/<str:judul>/', show_delete_song, name='show_delete_song')
+]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,68 @@
 from django.shortcuts import render
 
-# Create your views here.
+# def init_paket():
+    
+def show_langganan_paket(request):
+    paket = {
+        "1 Bulan": 20_000,
+        "3 Bulan": 50_000,
+        "6 Bulan": 100_000,
+        "1 Tahun": 150_000
+    }
+
+    context = {
+        'paket': paket
+    }
+
+    return render(request, "langganan_paket/langganan_paket.html", context)
+
+def show_finalisasi_paket(request, jenis, harga):
+    context = {
+        'jenis_paket': jenis,
+        'harga_paket': harga
+    }
+    return render(request, "langganan_paket/finalisasi_langganan.html", context)
+
+def show_riwayat_langganan(request):
+    riwayat_transaksi = [
+        ["1 Bulan", "8 April 2024, 23:00", "8 Mei 2024, 23:00", "E-Wallet", 20_000]
+        ]
+
+    context = {
+        'riwayat_transaksi': riwayat_transaksi
+    }
+
+    return render(request, "langganan_paket/riwayat_langganan.html", context)
+
+def show_hasil_cari(request, query):
+    daftar_lagu = [
+        ["SONG", "Love is in the air", "Henry Soedibjo"],
+        ["SONG", "What is love", "Narendra"],
+        ["PODCAST", "Love is Blind Pod", "Johan"],
+        ["USER PLAYLIST", "90s Love Songs", "Nano"]
+    ]
+
+    context = {
+        'query': query,
+        'daftar_lagu': daftar_lagu
+    }
+    return render(request, "search_content/hasil_cari.html", context)
+
+def show_downloaded_song(request):
+    daftar_lagu = {
+        "artis1": "song1",
+        "artis2": "song2",
+        "artis3": "song3",
+        "artis4": "song4"
+    }
+
+    context = {
+        'daftar_lagu': daftar_lagu
+    }
+    return render(request, "downloaded_song/downloaded_song.html", context)
+
+def show_delete_song(request, judul):
+    context = {
+        'judul': judul
+    }
+    return render(request, "downloaded_song/delete_song.html", context)
