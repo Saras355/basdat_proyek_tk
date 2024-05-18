3e3f2f69c1b2041532a4773c3e70dc0cdfebc40c
Henrysoed <henry.soedibjo@gmail.com>
2024-05-18 17:49:43 +0700

    Add podcast fitur

--- play_podcast/urls.py
+++ play_podcast/urls.py
@@ -3,7 +3,10 @@ from play_podcast.views import *
 
 app_name = 'podcast'
 
-urlpatterns = [   
+urlpatterns = [    
     path('chart-detail/<str:playlist_id>/', show_chart_detail, name='show_chart_detail'),
     path('chart-list', show_chart_list, name='show_chart_list'),
+    path('delete-podcast/<str:podcast_id>/', delete_podcast, name='delete_podcast'),
+    path('<str:podcast_id>/', show_detail_podcast, name='show_detail_podcast'),
+    path('', manage_podcasts, name='manage_podcasts'),
 ]
