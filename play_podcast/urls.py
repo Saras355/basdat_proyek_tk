afa98f6df3d815fbd866e2e44a15fde3295ae56a
Henrysoed <henry.soedibjo@gmail.com>
2024-05-18 17:48:04 +0700

    Add chart_list fitur

--- play_podcast/urls.py
+++ play_podcast/urls.py
@@ -3,6 +3,7 @@ from play_podcast.views import *
 
 app_name = 'podcast'
 
-urlpatterns = [
+urlpatterns = [   
     path('chart-detail/<str:playlist_id>/', show_chart_detail, name='show_chart_detail'),
+    path('chart-list', show_chart_list, name='show_chart_list'),
 ]
