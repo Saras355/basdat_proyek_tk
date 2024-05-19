32dac205ad636e83d61a1c898b717e05ba2fc5d0
saras355 <saraswati.widhisari355@gmail.com>
2024-05-19 21:38:36 +0700

    ki

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-# web: gunicorn tk_basdat.wsgi --log-file -
-web: waitress-serve --port=$PORT tk_basdat.wsgi:application
+web: gunicorn tk_basdat.wsgi --log-file -
+# web: waitress-serve --port=$PORT tk_basdat.wsgi:application
--- tk_basdat/settings.py
+++ tk_basdat/settings.py
@@ -4,18 +4,19 @@ from pathlib import Path
 import string
 import secrets
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', ''.join(secrets.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(50)))
+# SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', ''.join(secrets.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(50)))
 
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = ["*"]
 
-# ALLOWED_HOSTS= ["tkbasdat-kelompok1.up.railway.app", "127.0.0.1"]
-# CSRF_TRUSTED_ORIGINS= ['https://tkbasdat-kelompok1.up.railway.app', 'https://127.0.0.1']
+ALLOWED_HOSTS= ["successful-generosity-terakhir.up.railway.app", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS= ['https://successful-generosity-terakhir.up.railway.app/', 'https://127.0.0.1']
 
-# SECURE_CROSS_ORIGIN_OPENER_POLICY ='same-origin-allow-popups'
+SECURE_CROSS_ORIGIN_OPENER_POLICY ='same-origin-allow-popups'
 
 
 INSTALLED_APPS = [
@@ -125,13 +126,13 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Security settings
-SECURE_HSTS_SECONDS = 3600
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_BROWSER_XSS_FILTER = True
-X_FRAME_OPTIONS = 'DENY'
-SECURE_CONTENT_TYPE_NOSNIFF = True
+# SECURE_HSTS_SECONDS = 3600
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_HSTS_PRELOAD = True
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# SECURE_BROWSER_XSS_FILTER = True
+# X_FRAME_OPTIONS = 'DENY'
+# SECURE_CONTENT_TYPE_NOSNIFF = True
 
